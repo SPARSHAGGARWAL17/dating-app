d494b08d8a6473e32f8c803f430e04c75c4282d9
SPARSHAGGARWAL17 <aggarwalsparsh175@gmail.com>
2021-04-27 16:25:01 +0530

    asset setup

--- lib/const.dart
+++ lib/const.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xffFFB6C1);
+TextStyle buildTextStyle({
+  String family = 'SFProDisplay',
+  double size = 15,
+  FontWeight weight = FontWeight.normal,
+  Color color = Colors.black,
+}) {
+  return TextStyle(
+    fontFamily: family,
+    fontSize: size,
+    fontWeight: weight,
+    color: color,
+  );
+}
